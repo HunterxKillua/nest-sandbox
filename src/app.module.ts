b4be7f0f61cb87ec866dda6d209ed40ac434c6f1
wangchengjie <wcj199621714@outlook.com>
2023-10-26 17:31:36 +0800

    feat: restful style test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppMiddleware } from './app.middleware';
+import { PersonModule } from './person/person.module';
 
 @Module({
-  imports: [],
+  imports: [PersonModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// import { AppModule } from './app.module';
+import { PersonModule } from './person/person.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(PersonModule);
+  app.enableCors();
   await app.listen(9000);
 }
 bootstrap();
